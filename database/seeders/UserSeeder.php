9f485c90033d7dea6591860e348599be33d2830a
Andre Irawan <andreirawan99@gmail.com>
2023-09-27 16:52:04 +0700

    crud user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,5 +15,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+
+        User::create([
+            'name'=>'Andre Irawan',
+            'email'=>'andre@gmail.com',
+            'email_verified_at'=>now(),
+            'password'=>Hash::make('123'),
+            'roles'=>'',
+        ]);
     }
 }
